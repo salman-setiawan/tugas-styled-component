76b082dab94a18be3f5009ebe30e098f6362c702
Salman Setiawan <11191066@student.itk.ac.id>
2021-11-01 23:45:03 +0800

    .

---
+++ soal-01-dan-02/src/components/styles/FlexContainer.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const FlexContainer = styled.div`
+  display: flex;
+  height: 100vh;
+  flex-wrap: wrap;
+
+  flex-direction: ${(props) => props.direction ? props.direction : "row"};
+
+  justify-content: ${(props) => props.justify ? props.justify : "center"};
+
+  align-items: ${(props) => props.align ? props.align : "center"};
+`;
+
+export default FlexContainer
